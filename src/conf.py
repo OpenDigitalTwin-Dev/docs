6c2999ed42111265190cc9310f72badf076780d8
fengsim <jiping.xin@lsec.cc.ac.cn>
2025-05-16 08:37:06 +0800

    c

--- src/conf.py
+++ src/conf.py
@@ -58,12 +58,12 @@ html_theme_options = {
 #    'analytics_id': 'G-XXXXXXXXXX',  #  Provided by Google in your dashboard
 #    'analytics_anonymize_ip': False,
 #    'logo_only': False,
-#    'prev_next_buttons_location': 'bottom',
-#    'style_external_links': False,
-#    'vcs_pageview_mode': '',
+    'prev_next_buttons_location': 'top',
+    'style_external_links': False,
+    'vcs_pageview_mode': '',
     'style_nav_header_background': 'grey',
-#    'flyout_display': 'hidden',
-    'version_selector': False
+    'flyout_display': 'hidden',
+    'version_selector': True
 #    'language_selector': True,
     # Toc options
 #    'collapse_navigation': True,
